1f73f877c54a91127c0e3481e2685a2117cd075c
yongbeom.cho <yongbeom.cho@ikoob.com>
2025-05-29 07:57:38 +0900

    init

---
+++ src/main/java/com/cho/polio/PolioApplication.java
@@ -0,0 +1,13 @@
+package com.cho.polio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cho/polio/PolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cho.polio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
