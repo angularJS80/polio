19014eb7452e1f642d799110dfb4d8bebfbe4d5f
MeeyoungChoi <goodlife1359@gmail.com>
2025-05-31 13:54:43 +0900

    feat : 인메모리 로그인 제거 , 스프링 시큐리티가 토큰을 통한 인가는 keycloak에 요구하도록 변경, /sample/** 외 다른 요청은 인가된 요청만 허용

--- src/main/java/com/cho/polio/presentation/security/SecurityConfig.java
+++ src/main/java/com/cho/polio/presentation/security/SecurityConfig.java
@@ -1,23 +1,41 @@
 package com.cho.polio.presentation.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
+                .csrf(csrf -> csrf.disable()) // REST API인 경우 비활성화 권장
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/sample/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults());
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
+                        })
+                );
+
         return http.build();
     }
 }
