c466e50c71065277a058936ba5bc20a5a5419731
yongbeom.cho <unbone80@gmail.com>
2025-06-08 20:51:46 +0900

    feat : security 제거 , 보안 처리 및 정책은 keycloak과 gateway에거 담당

--- src/main/java/com/cho/polio/infrastructure/keycloak/dto/RoleConfig.java
+++
@@ -1,9 +0,0 @@
-package com.cho.polio.infrastructure.keycloak.dto;
-
-import lombok.Data;
-
-@Data
-public class RoleConfig {
-    private String id;
-    private boolean required;
-}
--- src/test/java/com/cho/polio/PolioApplicationTests.java
+++ src/test/java/com/cho/polio/PolioApplicationTests.java
@@ -1,19 +1,14 @@
 package com.cho.polio;
 
-import com.cho.polio.infrastructure.keycloak.client.KeycloakAdminClient;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PolioApplicationTests {
 
-    @Autowired
-    KeycloakAdminClient keycloakAdminClient;
 
     @Test
     void contextLoads() {
-        //List<IdentityInfo> scopeIdentiyList = keycloakAdminClient.retrieveScopeIdentityResourceId(resourceIdentityInfo.get_id());
     }
 
 }
