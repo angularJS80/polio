29cb43e129b7dae0b1c23fc83ac52c154e81c01d
yongbeom.cho <unbone80@gmail.com>
2025-06-09 20:13:32 +0900

    feat : security 영역 추가 추후 비교용

---
+++ src/main/java/com/cho/polio/presentation/security/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.cho.polio.presentation.security.config;
+
+import com.cho.polio.presentation.security.authroization.PermissionRuleUriMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final PermissionRuleUriMapper permissionRuleUriMapper;
+
+    @Bean
+    @Order(0)
+    public SecurityFilterChain publicWebFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher(permissionRuleUriMapper.getPublicSecurityMatcher())
+                .build();
+
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> {
+                    permissionRuleUriMapper.configureAuthorization(authz);
+                    authz.anyRequest().authenticated();
+                })
+                //.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakJwtAuthenticationConverter)))
+                .exceptionHandling(this::configureExceptionHandling);
+
+        return http.build();
+    }
+
+
+    private void configureExceptionHandling(ExceptionHandlingConfigurer<HttpSecurity> ex) {
+        ex.authenticationEntryPoint((request, response, authException) -> {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+        }).accessDeniedHandler((request, response, accessDeniedException) -> {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
+        });
+    }
+}
+
