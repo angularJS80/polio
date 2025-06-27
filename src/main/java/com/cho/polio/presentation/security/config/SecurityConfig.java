d8455610ea7369eb4354f1ece3f9b51f479a2526
yongbeom.cho <unbone80@gmail.com>
2025-06-27 22:55:47 +0900

    feat: cors 필터 설정(Security Base)

--- src/main/java/com/cho/polio/presentation/security/config/SecurityConfig.java
+++ src/main/java/com/cho/polio/presentation/security/config/SecurityConfig.java
@@ -7,10 +7,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
@@ -21,18 +27,35 @@ public class SecurityConfig {
     private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
 
     @Bean
-    @Order(0)
+    @Order(1)
     public SecurityFilterChain publicWebFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
                 .securityMatcher(permissionRuleUriMapper.getPublicSecurityMatcher())
                 .build();
 
     }
 
     @Bean
-    @Order(1)
+    @Order(0)
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);  // ←
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+    }
+
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authz -> {
