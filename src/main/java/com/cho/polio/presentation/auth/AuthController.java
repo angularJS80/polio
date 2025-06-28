8bf3484969d6e20656e5adac7f299b1171d9434d
yongbeom.cho <unbone80@gmail.com>
2025-06-28 23:36:29 +0900

    feat: 계정등록 , 비밀번호 변경 추가

--- src/main/java/com/cho/polio/presentation/auth/AuthController.java
+++ src/main/java/com/cho/polio/presentation/auth/AuthController.java
@@ -3,8 +3,10 @@ package com.cho.polio.presentation.auth;
 import com.cho.polio.presentation.auth.dto.RequestRefresh;
 import com.cho.polio.presentation.enums.ApiPaths;
 import com.polio.poliokeycloak.keycloak.helper.KeycloakAuthHelper;
+import com.polio.poliokeycloak.keycloak.helper.dto.UserChangePasswordRequest;
 import com.polio.poliokeycloak.keycloak.helper.dto.UserLoginRequest;
 import com.polio.poliokeycloak.keycloak.helper.dto.UserLoginResponse;
+import com.polio.poliokeycloak.keycloak.helper.dto.UserRegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,10 +35,23 @@ public class AuthController {
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<UserLoginResponse> signOut(@RequestBody RequestRefresh refreshToken) {
+    public ResponseEntity<Void> signOut(@RequestBody RequestRefresh refreshToken) {
 
         keycloakAuthHelper.signOut(refreshToken.getRefreshToken());
 
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<Void> join(@RequestBody UserRegisterRequest userRegisterRequest) {
+        keycloakAuthHelper.regist(userRegisterRequest);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PutMapping("/password")
+    public ResponseEntity<Void> changePassword(@RequestBody UserChangePasswordRequest userChangePasswordRequest) {
+        keycloakAuthHelper.changeUserPassword(userChangePasswordRequest);
         return ResponseEntity.ok().build();
     }
 
