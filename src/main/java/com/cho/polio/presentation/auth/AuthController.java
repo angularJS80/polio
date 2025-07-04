8f7e054cf97d278db1e7c238bdb4b26c2aab7e6e
yongbeom.cho <unbone80@gmail.com>
2025-07-05 00:04:07 +0900

    feat : 소셜로그인 추가, 비밀번호찾기 추가

--- src/main/java/com/cho/polio/presentation/auth/AuthController.java
+++ src/main/java/com/cho/polio/presentation/auth/AuthController.java
@@ -1,19 +1,26 @@
 package com.cho.polio.presentation.auth;
 
-import com.cho.polio.presentation.auth.dto.RequestPassword;
-import com.cho.polio.presentation.auth.dto.RequestRefresh;
-import com.cho.polio.presentation.auth.dto.RequestSendReset;
+import com.cho.polio.presentation.auth.dto.*;
 import com.cho.polio.presentation.enums.ApiPaths;
 import com.polio.poliokeycloak.keycloak.helper.KeycloakAuthHelper;
 import com.polio.poliokeycloak.keycloak.helper.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
 
 @RestController
 @RequestMapping(ApiPaths.AUTH)
@@ -56,14 +63,14 @@ public class AuthController {
     }
 
 
-    @PutMapping("/password")
+    @PutMapping("/change-password")
     public ResponseEntity<Void> changePassword(@RequestBody RequestPassword requestPassword,
                                                @AuthenticationPrincipal Jwt jwt) {
         keycloakAuthHelper.changeUserPassword(new UserChangePasswordRequest(jwt.getSubject(), requestPassword.getNewPassword()));
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/send-reset")
+    @PostMapping("/find-password")
     public ResponseEntity<Void> sendResetPassword(@RequestBody RequestSendReset requestSendReset) {
 
         String id =  keycloakAuthHelper.findUserByEmail(requestSendReset.getEmail());
@@ -79,6 +86,43 @@ public class AuthController {
         mailSender.send(message);
 
         return ResponseEntity.ok().build();
+    }
+
+
+    @GetMapping("/social-login")
+    public ResponseEntity<?> proxyToKeycloak(
+            @RequestParam String idp
+            ,@RequestParam String scope
+            ,@RequestParam String redirectUrl
+
+    ) {
+        String keycloakUrl =  keycloakAuthHelper.getOauthIdpLoginLink(new OauthLinkRequest(
+                idp
+                ,redirectUrl
+                , scope
+        ));
+        URI uri = URI.create(keycloakUrl);  //
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uri);
+        return new ResponseEntity<>(headers, HttpStatus.FOUND); // 302 Redirect
+    }
+
+
+    @PostMapping("/login-by-code")
+    public ResponseEntity<UserLoginResponse> loginByCodePost(@RequestBody RequestCode requestCode) {
+
+        CodeLoginRequest request = new CodeLoginRequest(requestCode.getCode(), "http://localhost:3000/auth/callback");
+        UserLoginResponse response = keycloakAuthHelper.refreshByCode(request);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/login-by-code")
+    public ResponseEntity<UserLoginResponse> loginByCodeRedirect(@RequestParam String code,@RequestParam(name = "redirect_uri", required = false) String redirectUri) {
+        CodeLoginRequest request = new CodeLoginRequest(code, "http://localhost:8080/auth/callback");
+        UserLoginResponse response = keycloakAuthHelper.refreshByCode(request);
+         return ResponseEntity.ok(response);
     }
 
 
