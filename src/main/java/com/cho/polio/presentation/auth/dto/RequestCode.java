0c870942a5ce4d1f21ae5006d2e922296cd7bb74
yongbeom.cho <unbone80@gmail.com>
2025-07-05 00:05:12 +0900

    feat : 소셜로그인 요청 모델 추가

---
+++ src/main/java/com/cho/polio/presentation/auth/dto/RequestCode.java
@@ -0,0 +1,11 @@
+package com.cho.polio.presentation.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestCode {
+    private String code;
+    // 생성자, getter, setter
+}
