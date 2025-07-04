05ca32595b45cc3b05a18713a30c8e4789f58cd1
yongbeom.cho <unbone80@gmail.com>
2025-07-05 00:04:46 +0900

    feat : 관리자 로그인 유지

--- src/main/java/com/cho/polio/PolioApplication.java
+++ src/main/java/com/cho/polio/PolioApplication.java
@@ -2,8 +2,10 @@ package com.cho.polio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PolioApplication {
 
     public static void main(String[] args) {
