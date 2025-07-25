811a48819063adfa87fb6cee93fff46910e80ae5
yongbeom.cho <unbone80@gmail.com>
2025-07-25 09:17:22 +0900

    feat : 어플리케이션 기본타임존 설정

--- src/main/java/com/cho/polio/PolioApplication.java
+++ src/main/java/com/cho/polio/PolioApplication.java
@@ -1,15 +1,27 @@
 package com.cho.polio;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class PolioApplication {
+    @Value("${spring.jackson.time-zone:UTC}")
+    private String jacksonTimeZone;
 
     public static void main(String[] args) {
         SpringApplication.run(PolioApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(jacksonTimeZone));
+        System.out.println("Set JVM Default TimeZone to: " + jacksonTimeZone);
     }
 
 }
