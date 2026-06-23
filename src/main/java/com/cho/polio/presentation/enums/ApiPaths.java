dfe98531533f5d8b07cf1a6735b0ec5c816a2d04
yongbeom.cho <yongbeom.cho@ikoob.com>
2026-06-23 14:32:45 +0900

    ApiPaths.java 파일에 UPLOAD API 경로 추가

--- src/main/java/com/cho/polio/presentation/enums/ApiPaths.java
+++ src/main/java/com/cho/polio/presentation/enums/ApiPaths.java
@@ -3,4 +3,5 @@ package com.cho.polio.presentation.enums;
 public class ApiPaths {
     public static final String SAMPLE = "/sample";
     public static final String AUTH = "/auth";
+    public static final String FILE = "/api/v1/files";
 }
